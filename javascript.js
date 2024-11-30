e4e322f7b1da06f9b6f1b8388d34c101683d638f
Albert <102751941+khantthureinzaw@users.noreply.github.com>
2024-11-30 21:38:34 +0700

    Introduce some modifications
    The squares now have random background colors.
    The opacity of the color also increase by 10% each time it is hovered on.

--- javascript.js
+++ javascript.js
@@ -12,9 +12,30 @@ function addHoverEffect() {
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'red';
-        })
-    })
+
+            if (!square.style.backgroundColor) {
+                square.style.backgroundColor = `rgba(${getRandColor()}, 0.1)`;
+            } else {
+
+                // if background color is already changed, increase opacity by 0.1
+                const bgColor = square.style.backgroundColor;
+                let parts = bgColor.slice(5, -1).split(',');
+                let opacity = parts[3];
+                parts[3] = parseFloat(opacity) + 0.1;
+                newBgColor = `rgba(${parts.join(', ')})`;
+                square.style.backgroundColor = newBgColor;
+            }
+
+        });
+    });
+}
+
+// Get a random color
+function getRandColor() {
+    let r = Math.floor(Math.random() * 255) + 1;
+    let g = Math.floor(Math.random() * 255) + 1;
+    let b = Math.floor(Math.random() * 255) + 1;
+    return `${r}, ${g}, ${b}`;
 }
 addHoverEffect();
 
@@ -24,7 +45,7 @@ resetBtn.addEventListener('click', () => {
     let num = 0;
     do {
         num = parseInt(prompt('How many squares per side would you like?'));
-    } while (!(Number.isInteger(num) && num <= 100));
+    } while (!(Number.isInteger(num) && num > 0 && num <= 100));
     buildGrid(num);
 })
 
